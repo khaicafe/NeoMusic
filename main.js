c89743b5473438b3d533cd34c92863b4fa33dec7
khaicafe <minhkhai.vng@gmail.com>
2023-09-01 17:46:43 +0700

    v1.1

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.setLoginItemSettings({
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
 // if (require('electron-squirrel-startup')) {
 //   app.quit();
 // }
